071d5199eb6a82ca52a7e376818c1b4ec4e6d56c
Maxwell <Maxwellbarboza@hotmail.com>
2025-04-11 14:47:56 -0300

    Alterando o readme e incluindo o dotenv

--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -1,10 +1,14 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const sequelize = new Sequelize({
-  username: "postgres",
-  password: "123456",
-  database: "desafio_tecnico",
-  host: "127.0.0.1",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   dialect: "postgres",
   logging: false,
 });
